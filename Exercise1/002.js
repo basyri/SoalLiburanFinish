48022e57ba7f854901f958caeb105335a72b8772
basyri <husain.basyri@gmail.com>
2019-04-07 20:06:03 +0700

    update

--- Exercise1/002.js
+++ Exercise1/002.js
@@ -1,20 +1,39 @@
 function soal2(param) {
-    var label = [
-        ["firtName:"],
-        ["lastName:"],
-        ["hobbies:"],
-        ["profession:"]
-    ]
-    var tampung = []
-    for (var i = 0; i < param.length; i++) {
-        tampung[i] = []
-        for (var j = 0; j < param.length + 2; j++) {
-            tampung[i].push(label[j] + param[i][j])
-        }
+    // =================
+    //    versi -array
+    // =================
+    // var label = [
+    //     ["firtName:"],
+    //     ["lastName:"],
+    //     ["hobbies:"],
+    //     ["profession:"]
+    // ]
+    // var tampung = []
+    // for (var i = 0; i < param.length; i++) {
+    //     tampung[i] = []
+    //     for (var j = 0; j < param.length + 2; j++) {
+    //         tampung[i].push(label[j] + param[i][j])
+    //     }
+
+    // }
+    // console.log('\n')
+    // return tampung
+    // =================
+    //   versi -object
+    // =================
+    var arr = []
+    for (let i = 0; i < param.length; i++) {
+        arr.push({
+            firtName: param[i][0],
+            lastName: param[i][1],
+            lastNhobbiesame: param[i][2],
+            profession: param[i][3]
+        })
 
     }
-    console.log('\n')
-    return tampung
+    console.log();
+
+    return arr
 }
 
 
@@ -45,3 +64,14 @@ console.log(soal2([
     ['Awtian', 'Akbar', ['coding', 'main kucing', 'anime'], 'Web Instructur'],
     ['Megalo', 'Maniak', ['metal music', 'maniac'], 'Maniac Expert']
 ]));
+/* 
+[ { firtName: 'Awtian',
+    lastName: 'Akbar',
+    lastNhobbiesame: [ 'coding', 'main kucing', 'anime' ],
+    profession: 'Web Instructur' },
+  { firtName: 'Megalo',
+    lastName: 'Maniak',
+    lastNhobbiesame: [ 'metal music', 'maniac' ],
+    profession: 'Maniac Expert' } ]
+
+*/
