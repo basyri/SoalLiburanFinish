e791ca1c95dd7d7630f469b6576492066899a508
basyri <husain.basyri@gmail.com>
2019-04-14 18:42:30 +0700

    update

--- Exercise2/003.js
+++ Exercise2/003.js
@@ -1,29 +1,51 @@
 function groupAnimals(animals) {
-    let arr = []
+    // ============
+    // versi object
+    // ============
+    // let arr = []
 
-    let obj = {}
-    for (let i = 0; i < animals.length; i++) {
-        if (obj[animals[i][0]] === undefined) {
-            obj[animals[i][0]] = 0
-        }
-        obj[animals[i][0]] += 1
+    // let obj = {}
+    // for (let i = 0; i < animals.length; i++) {
+    //     if (obj[animals[i][0]] === undefined) {
+    //         obj[animals[i][0]] = 0
+    //     }
+    //     obj[animals[i][0]] += 1
 
-    }
+    // }
 
-    let obKeys = Object.keys(obj)
-    obKeys.sort()
-    for (let j = 0; j < obKeys.length; j++) {
-        let cage = []
-        for (let k = 0; k < animals.length; k++) {
-            if (obKeys[j] === animals[k][0]) {
-                cage.push(animals[k])
+    // let obKeys = Object.keys(obj)
+    // obKeys.sort()
+    // for (let j = 0; j < obKeys.length; j++) {
+    //     let cage = []
+    //     for (let k = 0; k < animals.length; k++) {
+    //         if (obKeys[j] === animals[k][0]) {
+    //             cage.push(animals[k])
+    //         }
+    //     }
+    //     arr.push(cage)
+    //     // ifobKeys[j];
+
+    // }
+    // return arr
+    // ============
+    // versi kamus
+    // ============
+    var dic = 'abcdefghijklmnopqrstuvwxyz'
+    var result = []
+    for (let i = 0; i < dic.length; i++) {
+        // console.log(dic[i])
+        var cage = []
+        for (let j = 0; j < animals.length; j++) {
+            if (dic[i] === animals[j][0]) {
+                cage.push(animals[j])
             }
-        }
-        arr.push(cage)
-        // ifobKeys[j];
 
+        }
+        if (cage.length > 0) {
+            result.push(cage)
+        }
     }
-    return arr
+    return result
 }
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
